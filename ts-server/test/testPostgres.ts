ec52531d9fadf69b2b7b78ae99e8a6f618657d72
DJ5harma <sunubhargav2004@gmail.com>
2025-12-06 11:22:48 +0530

    update

--- ts-server/test/testPostgres.ts
+++ ts-server/test/testPostgres.ts
@@ -1,5 +1,33 @@
-import { sql } from "bun";
+import { SQL } from "bun";
 
 export async function testPostgres() {
-    const db = sql.connect()
+	const pg = new SQL("postgres://sunu:password@localhost:5432/room-coom");
+	await pg.connect();
+
+	await pg`DROP TABLE IF EXISTS whether;`;
+	await pg`DROP TABLE IF EXISTS cities`;
+
+	await pg`CREATE TABLE IF NOT EXISTS weather  (
+                city            varchar(80),
+                temp_lo         int,           
+                temp_hi         int,           
+                prcp            real,          
+                date            date
+        );`;
+
+	await pg`CREATE TABLE IF NOT EXISTS cities (
+                name            varchar(80),
+                location        point
+            );`;
+
+	const date = new Date();
+	await pg`INSERT INTO weather VALUES ('San Francisco', 46, 50, 0.25, ${
+		date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
+	});`;
+    await pg`INSERT INTO cities VALUES ('New York', '(-194.0, 53.0)');`
+
+    await pg`INSERT INTO weather (city, temp_lo, temp_hi, prcp, date) 
+                        VALUES   ()`
+
+	console.log("TEST");
 }
