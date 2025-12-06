1ff424b376fb4565870fca810721fa1f5bcac618
DJ5harma <sunubhargav2004@gmail.com>
2025-12-06 06:33:21 +0530

    update

--- ts-server/src/auth/GoogleAuth/google.auth.routes.ts
+++ ts-server/src/auth/GoogleAuth/google.auth.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { GoogleAuthController } from "./google.auth.controller";
+import { UserController } from "../../user/user.controller";
 
 const router = express.Router();
 
 router.get("/config", GoogleAuthController.getConfig);
-router.post("/signin", GoogleAuthController.signin);
+router.post("/signin", GoogleAuthController.signin, UserController.signin);
 
 export const GoogleAuthRouter = router;
