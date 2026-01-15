4dfefb7ab353f8a1db3cab41eb7ac47b2a1932bb
DJ5harma <sunubhargav2004@gmail.com>
2026-01-15 11:25:20 +0530

    cors

--- business-server/src/main.ts
+++ business-server/src/main.ts
@@ -2,11 +2,13 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { AppError } from "./error/AppError";
+import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
 	res.send("hello");
