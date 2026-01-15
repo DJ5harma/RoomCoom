28bcd1cad24e71dbf86176f7fb93baf5551f2c46
DJ5harma <sunubhargav2004@gmail.com>
2026-01-15 11:24:36 +0530

    basic setup

---
+++ business-server/src/error/AppError.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from "express";
+
+export class AppError extends Error {
+	statusCode: number;
+	constructor(statusCode: number, message: string) {
+        super(message);
+		this.statusCode = statusCode;
+	}
+    
+	static ExpressErrorHandler(
+        err: AppError,
+		_req: Request,
+		res: Response,
+		_next: NextFunction
+	) {
+        console.error("AppError:", err.message);
+		console.error(err.stack);
+		res.status(err.statusCode || 500).json({
+			success: false,
+			message: err.message || "Internal Server Error",
+		});
+	}
+}
---
+++ business-server/src/main.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { AppError } from "./error/AppError";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+app.get("/err", (req, res) => {
+	throw new Error("ERROR TEST ROUTE - OK");
+});
+
+app.use(AppError.ExpressErrorHandler);
+
+const PORT = 4000;
+server.listen(PORT, () => {
+	console.log(`listening on http://localhost:${PORT}`);
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+});
