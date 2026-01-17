a19ba22205120c4060a173fd2789f657e3f333af
DJ5harma <sunubhargav2004@gmail.com>
2026-01-17 14:44:33 +0530

    udpate

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Api } from "@/utils/Api";
 
 export default function Home() {
 	const [rooms, setRooms] = useState([]);
 
+	useEffect(() => {
+		Api.get("/manager/my-rooms").then(({ data }) => {
+			setRooms(data.rooms);
+		});
+	}, []);
 	function handleCreateRoom(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 		const formData = new FormData(e.currentTarget);
@@ -20,8 +25,14 @@ export default function Home() {
 
 	return (
 		<div className="flex min-h-screen items-center justify-center">
-			<form onSubmit={handleCreateRoom}>
-				<input type="text" name="roomName" />
+			{rooms.map((room) => {
+				return <>{JSON.stringify(room)}</>;
+			})}
+			<form
+				onSubmit={handleCreateRoom}
+				className="bg-white text-black flex flex-col p-4 gap-4"
+			>
+				<input type="text" name="roomName" placeholder="Room Name" />
 				<button>Create Room</button>
 			</form>
 		</div>
