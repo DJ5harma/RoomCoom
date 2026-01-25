0b1ff64a19778c795b81699592f601d7e52bd8ec
DJ5harma <sunubhargav2004@gmail.com>
2026-01-25 22:21:19 +0530

    test plugin

--- business-server/src/entities/container/container.routes.ts
+++ business-server/src/entities/container/container.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { ContainerController } from "./container.controller";
+import { testRouter } from "../plugins/test/test.routes";
 
 export const containerRouter = Router();
 
 containerRouter.post("/create", ContainerController.create);
-containerRouter.get('/:containerId', ContainerController.get)
+containerRouter.get("/:containerId", ContainerController.get);
+
+containerRouter.get("/:containerId/test", testRouter);
