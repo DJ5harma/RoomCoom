4e9331fc29b69af2262caff8334de602b4b04316
DJ5harma <sunubhargav2004@gmail.com>
2026-02-02 00:55:46 +0530

    update ui a bit

--- web/app/room/[roomId]/page.tsx
+++ web/app/room/[roomId]/page.tsx
@@ -4,28 +4,40 @@ import { TestPlugin } from "@/app/plugins/TestPlugin";
 import { useRoomData } from "./context/roomData.context";
 import { ContainerForm } from "@/app/components/forms/ContainerForm";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
 	const { room, containers, roomMembers } = useRoomData();
 
 	return (
 		<div>
-			POHOCH GYA
 			<p>{room.name}</p>
 			<ContainerForm />
-			CONTINAERS
-			{containers.map((c) => {
-				return (
-					<Link href={`/room/${room.id}/container/${c.id}`} key={c.id}>
-						{JSON.stringify(c)}
-					</Link>
-				);
-			})}
-			MEMBERS
-			{roomMembers.map((c) => {
-				return <div key={c.id}>{JSON.stringify(c)}</div>;
-			})}
-			<TestPlugin />
+			<div className="w-full flex justify-between">
+				<div>
+					CONTAINERS
+					{containers.map(({id, name}) => {
+						return (
+							<Link className="flex flex-col bg-green-800 p-4" href={`/room/${room.id}/container/${id}`} key={id}>
+								<p>
+									{name}
+								</p>
+							</Link>
+						);
+					})}
+				</div>
+					<TestPlugin />
+				<div>
+					MEMBERS
+					{roomMembers.map(({name, email, id, pictureUrl}) => {
+						return <div className="flex flex-col bg-blue-800 p-4" key={id}>
+							<p>{name}</p>
+							<p>{email}</p>
+							<Image src={pictureUrl} height={40} width={40} alt={name} />
+						</div>;
+					})}
+				</div>
+			</div>
 		</div>
 	);
 }
