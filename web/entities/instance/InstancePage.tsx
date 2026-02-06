a749ff2cd9772fc6b7508ff1f3fb15261e33b669
DJ5harma <sunubhargav2004@gmail.com>
2026-02-06 21:14:32 +0530

    update

--- web/entities/instance/InstancePage.tsx
+++ web/entities/instance/InstancePage.tsx
@@ -1,15 +1,26 @@
+"use client";
 import { uuid } from "@/utils/types";
 import { useParams } from "next/navigation";
 import { useFetchInstance } from "./useFetchInstance";
 import { Loading } from "@/components/Loading";
 import { NotFound } from "@/components/NotFound";
+import { useEffect } from "react";
+import { useInstanceMemory } from "./InstanceMemory";
+import { InstanceProvider } from "./InstanceProvider";
 
 export default function InstancePage() {
 	const { instanceId } = useParams() as { instanceId: uuid };
 
+	const { activateInstance, getShownNode } = useInstanceMemory();
 	const { instance, loading } = useFetchInstance(instanceId);
+
+	useEffect(() => {
+		if (!instance) return;
+		activateInstance(instance, <InstanceProvider instance={instance} />);
+	}, [instance, activateInstance]);
 
 	if (loading) return <Loading />;
 	if (!instance) return <NotFound />;
-	return <div>InstancePage</div>;
+
+	return <>{getShownNode()}</>;
 }
