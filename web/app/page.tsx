dead0a4c90ae892eeeaec67d3e076cfce54773e5
DJ5harma <sunubhargav2004@gmail.com>
2026-02-07 16:05:04 +0530

    udpate

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,24 +1,15 @@
 "use client";
-import Link from "next/link";
 import { DirectsBar } from "./DirectsBar";
 import { RoomsBar } from "./RoomsBar";
 
 export default function Page() {
 	return (
-		<div className="w-full h-screen flex items-center justify-around">
+		<div className="w-full h-screen flex items-center">
 			<div className="h-full">
 				<DirectsBar />
 			</div>
 			<div className="h-full">
 				<RoomsBar />
-			</div>
-			<div className="flex-1">
-				<Link
-					href={"/room"}
-					className="bg-black text-white w-full h-full flex justify-center items-center"
-				>
-					ROOM MODE
-				</Link>
 			</div>
 		</div>
 	);
