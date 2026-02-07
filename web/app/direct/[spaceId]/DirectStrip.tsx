b83e708451ca9526e553193eda1d297173ae5ae7
DJ5harma <sunubhargav2004@gmail.com>
2026-02-07 19:12:41 +0530

    remove unused and update color

--- web/app/direct/[spaceId]/DirectStrip.tsx
+++ web/app/direct/[spaceId]/DirectStrip.tsx
@@ -7,7 +7,7 @@ export const DirectStrip = () => {
 	} = useSpace();
 
 	return (
-		<div className="w-full flex items-center bg-red-900 p-1 gap-5">
+		<div className="w-full flex items-center bg-blue-900 p-1 gap-5">
 			<p>{name}</p>
 			<div className="flex gap-1 bg-black rounded-xl p-1">
 				{members.map((user) => {
--- web/app/direct/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useSpace } from "@/entities/space/SpaceProvider";
-
-export default function Page() {
-	const { space } = useSpace();
-
-	return <div>{space.name} DIRECT </div>;
-}
