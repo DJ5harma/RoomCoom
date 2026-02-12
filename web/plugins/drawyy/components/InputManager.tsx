3d5b7f39f680f52c1656a496bfc0366f7389595d
DJ5harma <sunubhargav2004@gmail.com>
2026-02-13 02:24:12 +0530

    update

--- web/plugins/drawyy/components/InputManager.tsx
+++ web/plugins/drawyy/components/InputManager.tsx
@@ -2,13 +2,15 @@ import { useToolSelection } from "../providers/ToolSelectionProvider";
 import { CircleMaker } from "../makers/CircleMaker";
 import { useUser } from "@/entities/user/UserProvider";
 
+const now = () => Date.now();
+
 export const InputManager = () => {
 	const { selectedTool } = useToolSelection();
 	const { user } = useUser();
 
 	return (
 		<div className="w-full h-full border z-50 absolute top-0 left-0">
-			<CircleMaker myKey={`${user.id}:circle:maker`} />
+			<CircleMaker myKey={`${user.id}:circle:maker:${now()}`} />
 		</div>
 	);
 };
