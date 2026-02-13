18c4988ace500c8a7acff01e5eb360718011cad4
DJ5harma <sunubhargav2004@gmail.com>
2026-02-13 21:30:55 +0530

    fix rectangle

--- web/plugins/drawyy/makers/RectangleMaker.tsx
+++ web/plugins/drawyy/makers/RectangleMaker.tsx
@@ -1,25 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useElements } from "../providers/ElementsProvider";
 import { RectangleI, ElementI, Vec2 } from "../types";
 import { useContainer } from "../providers/ContainerProvider";
 
 export const RectangleMaker = () => {
-	const { getElement, updateElement, completeElement } = useElements();
+	const { updateElement, completeElement } = useElements();
 	const { correctElementPosition } = useContainer();
 
 	const name: ElementI["name"] = "rectangle";
 
 	const [isMaking, setIsMaking] = useState(false);
 
+	const startRef = useRef<Vec2 | null>(null);
+
 	function updateMe(circle: Omit<RectangleI, "name">) {
 		updateElement({ name, ...circle });
 	}
 	function completeMe() {
 		completeElement();
-	}
-
-	function getMe() {
-		return getElement()! as RectangleI;
 	}
 
 	return (
@@ -29,24 +27,34 @@ export const RectangleMaker = () => {
 				let position = [e.clientX, e.clientY] as Vec2;
 				position = correctElementPosition(position);
 
+				startRef.current = position;
+
 				updateMe({ position, dims: { w: 0, h: 0 } });
 				setIsMaking(true);
 			}}
 			onMouseMove={(e) => {
-				if (!isMaking) return;
-				let position = [e.clientX, e.clientY] as Vec2;
-				position = correctElementPosition(position);
+				if (!isMaking || !startRef.current) return;
 
-				const me = getMe();
-				me.dims = {
-					w: -(me.position[0] - position[0]),
-					h: -(me.position[1] - position[1]),
-				};
-				updateMe(me);
+				let current = [e.clientX, e.clientY] as Vec2;
+				current = correctElementPosition(current);
+
+				const start = startRef.current;
+
+				const left = Math.min(start[0], current[0]);
+				const top = Math.min(start[1], current[1]);
+
+				const w = Math.abs(current[0] - start[0]);
+				const h = Math.abs(current[1] - start[1]);
+
+				updateMe({
+					position: [left, top],
+					dims: { w, h },
+				});
 			}}
 			onMouseUp={() => {
 				completeMe();
 				setIsMaking(false);
+				startRef.current = null;
 			}}
 		/>
 	);
