bf45feb4e882b4bc26074e24ea2b800d1c1c4a25
DJ5harma <sunubhargav2004@gmail.com>
2026-02-13 13:32:03 +0530

    will do without konva, myself

--- web/plugins/drawyy/Drawyy.tsx
+++ web/plugins/drawyy/Drawyy.tsx
@@ -1,4 +1,3 @@
-import { Circle, Layer, Stage } from "react-konva";
 import { useContainer } from "./providers/ContainerProvider";
 import { useElements } from "./providers/ElementsProvider";
 import { CircleI } from "./types";
@@ -9,26 +8,8 @@ export const Drawyy = () => {
 	const { elements } = useElements();
 
 	return (
-		<Stage width={containerSize.width} height={containerSize.height} className="absolute inset-0">
-			<Layer>
-				{Object.entries(elements).map(([key, { element }]) => {
-					switch (element.name) {
-						case "circle":
-							const circle = element as CircleI;
-							return (
-								<Circle
-									key={key}
-									radius={circle.radius}
-									fill="red"
-									x={circle.position.x}
-									y={circle.position.y}
-								/>
-							);
-						default:
-							break;
-					}
-				})}
-			</Layer>
-		</Stage>
+		<div className={`w-[${containerSize.width}]px h-[${containerSize.height}]`}>
+
+		</div>
 	);
 };
