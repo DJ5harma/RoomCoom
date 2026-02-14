126fdc073626438edb432b96e6248e24dd1cc0a3
DJ5harma <sunubhargav2004@gmail.com>
2026-02-14 23:55:20 +0530

    update

--- web/components/Auth.tsx
+++ web/components/Auth.tsx
@@ -1,14 +1,21 @@
 import { API_URL } from "@/utils/Api";
+import Image from "next/image";
 
 export const Auth = () => {
 	return (
-		<div className="flex justify-center items-center min-h-screen">
+		<div className="flex justify-center items-center min-h-screen bg-linear-to-r p-2 flex-col gap-6">
+			<Image src={'/logo.png'} width={100} height={100} alt="Logo" />
+			<h3 className="text-5xl font-bold">Welcome to RoomCoom</h3>
+			<p className="text-xl">A multi-space plugin based collaboration project</p>
+			<code className="border-dotted border-b-2">Just for fun</code>
+
 			<a
-				className="bg-white text-black p-2"
+				className="bg-white text-black p-4 rounded-xl"
 				href={`${API_URL}/api/auth/google`}
 			>
 				Continue With Google
 			</a>
+			<a target="_blank" className="bg-linear-to-r from-neutral-700 to-neutral-900 p-2 rounded-xl" href="https://github.com/DJ5harma/RoomCoom">github.com/DJ5harma/RoomCoom</a>
 		</div>
 	);
 };
