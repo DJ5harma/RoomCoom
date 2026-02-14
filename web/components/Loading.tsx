8edcbea3c516eed34b42a550635de0040f5cb0b7
DJ5harma <sunubhargav2004@gmail.com>
2026-02-14 16:49:00 +0530

    big loading

--- web/components/Loading.tsx
+++ web/components/Loading.tsx
@@ -1,5 +1,7 @@
-export const Loading = ({message} : {message?: string}) => {
-  return (
-    <div>{message ?? "Loading..."}</div>
-  )
-}
+export const Loading = ({ message }: { message?: string }) => {
+	return (
+		<div className="w-full h-screen flex justify-center items-center">
+			<h2 className="text-4xl font-semibold">{message ?? "Loading..."}</h2>
+		</div>
+	);
+};
