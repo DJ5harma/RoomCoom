9fbda6af34332609bc22703d134ad10f7a88f159
DJ5harma <sunubhargav2004@gmail.com>
2026-02-14 17:48:51 +0530

    cool looiking gradients

--- web/components/Loading.tsx
+++ web/components/Loading.tsx
@@ -1,7 +1,7 @@
 export const Loading = ({ message }: { message?: string }) => {
 	return (
-		<div className="w-full h-screen flex justify-center items-center">
-			<h2 className="text-4xl font-semibold">{message ?? "Loading..."}</h2>
+		<div className="w-full h-screen flex justify-center items-center bg-linear-to-r from-red-950 to-orange-700">
+			<h2 className="text-4xl font-semibold animate-spin -rotate-45">{message ?? "Loading..."}</h2>
 		</div>
 	);
 };
--- web/plugins/_defaultyy/DefaultyyPlugin.tsx
+++ web/plugins/_defaultyy/DefaultyyPlugin.tsx
@@ -1,6 +1,6 @@
 export const DefaultyyPlugin = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-full">
+		<div className="flex flex-col items-center justify-center h-full bg-linear-to-r from-cyan-700 to-blue-800 animate-pulse">
 			<h2 className="text-3xl font-bold">Welcome!</h2>
 			<h3 className="text-xl">
 				Please choose a plugin to do something from the sidebar {`-> `}
